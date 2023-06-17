439db477ec1883d2ac359c521413a25663ef95fb
silvaalejesus <silvaalejesus@gmail.com>
2023-06-16 21:47:19 -0300

    added cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./router');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 // aqui diz que é pro arquivo app usar sempre o router. Entao toda requisição que aocntecer na aplicação vai cair no router
 app.use(router);
 
